5f9dec9f27e77f1561883365fcd9f3a8dad41e24
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-05-04 12:33:32 -0400

    initial commit

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float screenWidth = 16f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePosition = Input.mousePosition.x / Screen.width * screenWidth;
+        Vector2 paddlePosition = new Vector2(mousePosition, 0.35f);
+        transform.position = paddlePosition;
+    }
+}
